230bdd8db0fcd4da31a67e157b1eb0b3cda1ba00
Gísli Guðmundsson <gisli.gudmundsson@advania.is>
2018-10-19 20:41:20 +0000

    Maybe final

--- DocumentRetrieval.py
+++ DocumentRetrieval.py
@@ -3,17 +3,23 @@ import string
 # (1) Type in the document
 def open_file_stream():
     try:
+        # Allows the user to type in a document name
         filename = input("Document collection: ")
         #filename = "./skilaverkefni8/ap_docs.txt"
         file_string = ""
+        # if the file exists then read it into a file_stream
         with open(filename) as file_stream:
+            # Set the contents of the file into a variable and close the file
             file_string = file_stream.read()
     except FileNotFoundError:
         file_string = None
+
+    # Return the contents of the variable empty or not
     return file_string
 
 # (2) Select from menu
 def menu_selection():
+    # Users selection menu
     print("What would you like to do?")
     print("1. Search Documents")
     print("2. Print Document")
@@ -21,52 +27,69 @@ def menu_selection():
     selection = input("> ")
     return selection
 
+# (3) Create a dictionary from the contents of a file
 def create_word_dict(text_content):
     word_dict = {}
-
+    # Convert the text content to a list of documents
     document_list = convert_content_to_documents(text_content)
     for index, value in enumerate(document_list):
+        # remove returns and split the words based on empty string
         words = value.strip().split()
         for word in words:
+            # set the word to lower and remove all the punctiations
             word = word.lower().strip(string.punctuation)
             if word not in word_dict:
+                # if the key does not exist, add a new set into a key with the value of index
                 word_dict[word] = {index}
             else:
+                # if the key exists, append to the set
                 word_dict[word].add(index)
                 
     return word_dict
 
-# (2.1) Search Documents eftir viðeigandi orðum út frá bili
+# (2.1) Search Documents based on user input
 def search_documents(word_dict):
+    # User types in search words
     search_list = input("Enter search words: ")
+    # Splits the words based on space
     search_list = search_list.split()
-    for s in search_list:
-        if s in word_dict:
-            documents_found = ' '.join(str(n) for n in word_dict[s])
+    for search_string in search_list:
+        if search_string in word_dict:
+            # if the word exists as a key in the word_dict then print it out the set
+            documents_found = ' '.join(str(n) for n in word_dict[search_string])
             print("Documents that fit search: {}".format(documents_found))
         else:
+            # if there is no match to the search then report no match
             print("No match.")
-    #print(found_in_docs)
 
 # (2.2) Print out the selection of the document
 def print_document_number(text_content):
+    # User selects the document number
     select_doc_number = int(input("Enter document number: "))
     document_list = convert_content_to_documents(text_content)
-    document_printout = document_list[select_doc_number]
-    print("Documnent #{}{}".format(select_doc_number,document_printout))
+    try:
+        document_printout = document_list[select_doc_number]
+        print("Documnent #{}{}".format(select_doc_number,document_printout))
+    except IndexError:
+        # If the document does not exist it will give out an error
+        print("Document does not exist.")
 
-# (3) Convert the contents into documents
+# (4) Convert the contents into documents
 def convert_content_to_documents(file_contents):
+    # Convert the text_content to a multiple documents
     document_list = file_contents.split("<NEW DOCUMENT>")
     return document_list
 
-# Read Article
+# Main function
 def main():
     text_content = open_file_stream()
     if text_content is None:
+        # If the return is none then the document was not found
         print("Documents not found.")
+        # returns and exists the program
         return
     else:
+        # Creates a dictionary for the text content
         word_dict = create_word_dict(text_content)
     while True:
         selection = menu_selection()
